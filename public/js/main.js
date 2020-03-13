f7fb47a3c67f0d4242dc3859c8bb33ecded7b74f
Fabio Teixeira <fabio.teixeira@ifsp.edu.br>
2020-03-13 14:49:27 -0300

    Publicar aplicação no GitHub

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-angular.module('ifsp', ['ngRoute']).config(function($routeProvider) {
+angular.module('ifsp', ['ngRoute', 'ngResource']).config(function($routeProvider) {
 	
 	$routeProvider.when('/contatos', {
 		templateUrl: 'partials/contatos.html',
