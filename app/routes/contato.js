89f8b445c69cf5e64f73086a8e16f89554c9f16b
Victor Paes <victor.paes@aluno.ifsp.edu.br>
2022-05-19 12:37:25 -0300

    adjust

--- app/routes/contato.js
+++ app/routes/contato.js
@@ -1,4 +1,4 @@
-/*function verificaAutenticacao(req, res, next) {
+function verificaAutenticacao(req, res, next) {
     if (req.isAuthenticated()) {
     return next();
     } else {
@@ -14,14 +14,4 @@ module.exports = function(app) {
     app.route('/contatos/:id')
         .get(verificaAutenticacao, controller.obtemContato)
         .delete(verificaAutenticacao, controller.removeContato);
-};*/
-
-module.exports = function(app) {
-    var controller = app.controllers.contato;
-    app.route('/contatos')
-        .get(controller.listaContatos)
-        .post(controller.salvaContato);
-    app.route('/contatos/:id')
-        .get(controller.obtemContato)
-        .delete(controller.removeContato);
 };
