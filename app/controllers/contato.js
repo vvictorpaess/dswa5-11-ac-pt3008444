dd1c513096b627919103906ad4b2ddd53c27a711
Victor Paes <victor.paes@aluno.ifsp.edu.br>
2022-05-19 13:00:40 -0300

    adjusting

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,21 +1,6 @@
-function verificaAutenticacao(req, res, next) {
-   if (req.isAuthenticated()) {
-   return next();
-   } else {
-   res.status('401').json('Não autorizado');
-   }
-}
-
 module.exports = function (app) {
    var Contato = app.models.contato;
    var controller = app.controllers.contato;
-
-   app.route('/contatos')
-      .get(verificaAutenticacao, controller.listaTodos)
-      .post(verificaAutenticacao, controller.salvaContato);
-   app.route('/contatos/:id')
-      .get(verificaAutenticacao, controller.obtemContato)
-      .delete(verificaAutenticacao, controller.removeContato);
 
    controller.listaContatos = function (req, res) {
       Contato.find()
